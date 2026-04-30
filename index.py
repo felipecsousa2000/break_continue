5d037e4c2a7dfe760a2909eaaa9a440dfd325dba
felipecsousa2000 <132520533+felipecsousa2000@users.noreply.github.com>
2026-04-29 21:48:34 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,152 @@
+# contador = 1
+
+# while contador <= 10:
+#     print(contador)
+#     if contador == 5:
+#         break
+#     contador +=1
+
+# senha = ""
+# while True:
+#     senha = input("Digite a senha: ")
+
+#     if senha == "1234":
+#         print("Acesso liberado")
+#         break
+#     print("Senha incorreta")
+
+# ####continue
+# numero = 1
+# while numero <= 5:
+#     if numero == 2:
+#         numero +=1
+#         continue
+#     print(numero)
+#     numero +=1
+# contador = 0
+# while contador < 10:
+#     contador+=1
+#     if contador % 2 ==1:
+#         continue
+#     print(contador)
+
+# #atividade 01
+# numero = 1
+# while numero <= 10:
+#     print(numero)
+
+#     if numero == 6:
+#         break
+#     numero += 1,
+
+#atividade 02
+# numero = 1
+# while numero <=10:
+#     if numero == 5:
+#         numero += 1
+#         continue
+#     print(numero)
+#     numero+= 1
+
+#atividade 03
+# numero = 1
+# while numero <= 20:
+#     if numero == 17:
+#         break
+#     if numero % 2 == 0:
+#         numero +=1
+#         continue
+#     print(numero)
+#     numero +=1
+
+# # atividade 04
+# while True:
+#     produto = input("Digite o nome do produto:")
+
+#     if produto == "fim":
+#         print("cadastro finalizado")
+#         break
+#     print("Produto Cadastrado", produto)
+
+#atividade 05
+# soma = 0
+# while True:
+#     numero = int(input("Digite um numero: "))
+#     soma += numero
+
+#     if soma >= 20:
+#         break
+# print("Total",soma )
+# #a6
+# soma = 0
+# while True:
+#     numero = int(input("Digite um número: "))
+#     soma += numero
+
+#     if soma >= 50:
+#         print("Limite atingido!")
+#         print("Total:", soma)
+#         break
+
+#atividade 07
+while True:
+    senha = input("Digite a senha: ")
+
+    if senha == "teste":
+        print("Acesso liberado")
+        break
+    else:
+        print("senha incorreta")
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
